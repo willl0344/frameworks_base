cffc991d645b68e4a25738b729823d9cf48723d5
will <will@will-VirtualBox.(none)>
2013-11-13 19:13:11 -0500

    Tweak volume steps

--- media/java/android/media/AudioService.java
+++ media/java/android/media/AudioService.java
@@ -203,16 +203,16 @@ public class AudioService extends IAudioService.Stub {
 
    /** @hide Maximum volume index values for audio streams */
     private static final int[] MAX_STREAM_VOLUME = new int[] {
-        5,  // STREAM_VOICE_CALL
-        7,  // STREAM_SYSTEM
-        7,  // STREAM_RING
-        15, // STREAM_MUSIC
-        7,  // STREAM_ALARM
-        7,  // STREAM_NOTIFICATION
-        15, // STREAM_BLUETOOTH_SCO
-        7,  // STREAM_SYSTEM_ENFORCED
-        15, // STREAM_DTMF
-        15  // STREAM_TTS
+        10,  // STREAM_VOICE_CALL
+        10,  // STREAM_SYSTEM
+        10,  // STREAM_RING
+        30, // STREAM_MUSIC
+        10,  // STREAM_ALARM
+        10,  // STREAM_NOTIFICATION
+        30, // STREAM_BLUETOOTH_SCO
+        10,  // STREAM_SYSTEM_ENFORCED
+        10, // STREAM_DTMF
+        10  // STREAM_TTS
     };
     /* mStreamVolumeAlias[] indicates for each stream if it uses the volume settings
      * of another stream: This avoids multiplying the volume settings for hidden
