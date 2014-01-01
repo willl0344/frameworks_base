7459b465ae2f3220172c01c3393b193d4d863ae8
David96 <xd_e-mail@gmx.de>
2014-01-01 14:53:04 +0100

    Partly fix lockscreen blur rotation issue
    This works very well when changing rotation from portrait to landscape and vice versa but doesn't work when changing rotation from landscape to landscape (90 to 270 deg) or portrait to portrait (0 to 180 deg) because we don't get notified of such changes.
    
    Change-Id: Id4d9b4a6ce061fdb4767a8cb12dd7a8d33c99175

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -36,6 +36,7 @@ import android.content.ContentResolver;
 import android.database.ContentObserver;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
+import android.graphics.Matrix;
 import android.graphics.PixelFormat;
 import android.graphics.PorterDuff;
 import android.graphics.Rect;
@@ -101,11 +102,12 @@ public class KeyguardViewManager {
     private Bitmap mBlurredImage = null;
     private int mBlurRadius = 12;
     private boolean mSeeThrough = false;
+    private boolean mIsCoverflow = true;
 
     private KeyguardUpdateMonitorCallback mBackgroundChanger = new KeyguardUpdateMonitorCallback() {
         @Override
         public void onSetBackground(Bitmap bmp) {
-            mKeyguardHost.setCustomBackground( new BitmapDrawable(mContext.getResources(), 
+            mKeyguardHost.setCustomBackground( new BitmapDrawable(mContext.getResources(),
                         bmp != null ? bmp : mBlurredImage) );
             updateShowWallpaper(bmp == null && mBlurredImage == null);
         }
@@ -335,8 +337,8 @@ public class KeyguardViewManager {
 
             final int vWidth = getWidth();
             final int vHeight = getHeight();
- 
-            if (bgWidth == vWidth && bgHeight == vHeight) {
+
+            if (!mIsCoverflow) {
                 background.setBounds(0, 0, vWidth, vHeight);
                 return;
             }
@@ -390,7 +392,7 @@ public class KeyguardViewManager {
     }
 
     SparseArray<Parcelable> mStateContainer = new SparseArray<Parcelable>();
-
+    int mLastRotation = 0;
     private void maybeCreateKeyguardLocked(boolean enableScreenRotation, boolean force,
             Bundle options) {
         if (mKeyguardHost != null) {
@@ -442,13 +444,25 @@ public class KeyguardViewManager {
         }
 
         if(mBlurredImage != null) {
+            int currentRotation = mKeyguardView.getDisplay().getRotation() * 90;
+            mBlurredImage = rotateBmp(mBlurredImage, mLastRotation - currentRotation);
+            mLastRotation = currentRotation;
+            mIsCoverflow = false;
             KeyguardUpdateMonitor.getInstance(mContext).dispatchSetBackground(mBlurredImage);
+        } else {
+            mIsCoverflow = true;
         }
 
         updateUserActivityTimeoutInWindowLayoutParams();
         mViewManager.updateViewLayout(mKeyguardHost, mWindowLayoutParams);
 
         mKeyguardHost.restoreHierarchyState(mStateContainer);
+    }
+
+    private Bitmap rotateBmp(Bitmap bmp, int degrees) {
+        Matrix m = new Matrix();
+        m.postRotate(degrees);
+        return Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(), bmp.getHeight(), m, true);
     }
 
     private void inflateKeyguardView(Bundle options) {
