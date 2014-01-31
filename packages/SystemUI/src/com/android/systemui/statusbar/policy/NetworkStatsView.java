0292689016f76045ca73620b9021e65c77cbd49d
will <will@will-VirtualBox.(none)>
2014-01-31 13:54:47 -0500

    Revert "NetStats: Don't update stats when screen is off"
    This reverts commit f9f2950a17ce203ea0beeaaf85d8b34745a9a995.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkStatsView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkStatsView.java
@@ -26,7 +26,6 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.TrafficStats;
 import android.os.Handler;
-import android.os.PowerManager;
 import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.View;
@@ -65,6 +64,10 @@ public class NetworkStatsView extends LinearLayout {
         mLastTx = TrafficStats.getTotalTxBytes();
         mHandler = new Handler();
         mSettingsObserver = new SettingsObserver(mHandler);
+        mSettingsObserver.observe();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+        context.registerReceiver(mConnectivityReceiver, filter);
     }
 
     // runnable to invalidate view via mHandler.postDelayed() call
@@ -84,7 +87,7 @@ public class NetworkStatsView extends LinearLayout {
         }
 
         public void observe() {
-            final ContentResolver resolver = mContext.getContentResolver();
+            ContentResolver resolver = mContext.getContentResolver();
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_NETWORK_STATS), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
@@ -92,24 +95,16 @@ public class NetworkStatsView extends LinearLayout {
             onChange(true);
         }
 
-        public void unobserver() {
-            mContext.getContentResolver().unregisterContentObserver(this);
-        }
-
         @Override
         public void onChange(boolean selfChange) {
             // check for connectivity
             ConnectivityManager cm =
-                    (ConnectivityManager)mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+                    (ConnectivityManager)getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+
             NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
             boolean networkAvailable = activeNetwork != null ? activeNetwork.isConnected() : false;
-
-            PowerManager pm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
-            boolean isScreenOn = pm.isScreenOn();
-
             mActivated = (Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.STATUS_BAR_NETWORK_STATS, 0)) == 1
-                    && networkAvailable && isScreenOn;
+                    Settings.System.STATUS_BAR_NETWORK_STATS, 0)) == 1 && networkAvailable;
 
             mRefreshInterval = Settings.System.getLong(mContext.getContentResolver(),
                     Settings.System.STATUS_BAR_NETWORK_STATS_UPDATE_INTERVAL, 500);
@@ -122,11 +117,14 @@ public class NetworkStatsView extends LinearLayout {
         }
     }
 
-    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+    private BroadcastReceiver mConnectivityReceiver = new BroadcastReceiver() {
 
         @Override
         public void onReceive(Context context, Intent intent) {
-            mSettingsObserver.onChange(true);
+            String action = intent.getAction();
+            if (ConnectivityManager.CONNECTIVITY_ACTION.equals(action)) {
+                mSettingsObserver.onChange(true);
+            }
         }
     };
 
@@ -144,16 +142,6 @@ public class NetworkStatsView extends LinearLayout {
             mAttached = true;
             mHandler.postDelayed(mUpdateRunnable, mRefreshInterval);
         }
-
-        // register the broadcast receiver
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
-        filter.addAction(Intent.ACTION_SCREEN_ON);
-        filter.addAction(Intent.ACTION_SCREEN_OFF);
-        mContext.registerReceiver(mBroadcastReceiver, filter);
-
-        // start observing our settings
-        mSettingsObserver.observe();
     }
 
     @Override
@@ -162,12 +150,6 @@ public class NetworkStatsView extends LinearLayout {
         if (mAttached) {
             mAttached = false;
         }
-
-        // unregister the broadcast receiver
-        mContext.unregisterReceiver(mBroadcastReceiver);
-
-        // stop listening for settings changes
-        mSettingsObserver.unobserver();
     }
 
     private void updateStats() {
