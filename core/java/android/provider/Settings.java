90e91c7d6b46c62b3960e7ea8a1d18b72ec9cb34
will <will@will-VirtualBox.(none)>
2014-01-31 13:54:57 -0500

    Revert "Add network usage stats to status bar (1/2)"
    This reverts commit 95dd04d4279e77cd4b6b660036af015ed5b2d073.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3093,18 +3093,6 @@ public final class Settings {
          */
         public static final String QS_COLLAPSE_PANEL = "qs_collapse_panel";
 
-	/**
-         * Whether to show the network status in the status bar
-         * @hide
-         */
-        public static final String STATUS_BAR_NETWORK_STATS = "status_bar_network_stats";
- 
-        /**
-         * Frequency at which stats are updated, in milliseconds
-         * @hide
-         */
-        public static final String STATUS_BAR_NETWORK_STATS_UPDATE_INTERVAL = "status_bar_network_stats_update_frequency";
-
         /**
          * Quick Settings Smart Pulldown
          *
