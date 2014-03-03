969936fdbcbe181f18aa5121c4d7ceb707776e97
willl0344 <wgangers@gmail.com>
2014-03-03 17:32:21 -0500

    Revert "One more clear all recents fix, again"
    This reverts commit c0c354426bb31714bd274c4b295ffcec2e2623d2.

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -27,7 +27,6 @@ import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.ComponentName;
 import android.content.Intent;
-import android.content.pm.IPackageDataObserver;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
