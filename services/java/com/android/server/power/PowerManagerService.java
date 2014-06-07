3f0d4190c0a7f979d5dda9f1c749b0f704c5c25d
willl0344 <wgangers@gmail.com>
2014-06-07 16:29:37 -0400

    Revert "[1/2] Frameworks: Wakelock Blocker"
    This reverts commit be0469da9dcf3e89a8794cc829348e09353b235f.
    
    Conflicts:
    	core/java/android/os/IPowerManager.aidl
    	services/java/com/android/server/power/PowerManagerService.java

--- services/java/com/android/server/power/PowerManagerService.java
+++ services/java/com/android/server/power/PowerManagerService.java
@@ -65,9 +65,6 @@ import android.view.WindowManagerPolicy;
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Set;
-import java.util.HashSet;
-import java.util.Iterator;
 
 import libcore.util.Objects;
 
@@ -217,9 +214,6 @@ public final class PowerManagerService extends IPowerManager.Stub
 
     // Table of all wake locks acquired by applications.
     private final ArrayList<WakeLock> mWakeLocks = new ArrayList<WakeLock>();
-    private Set<String> mSeenWakeLocks = new HashSet<String>();
-    private Set<String> mBlockedWakeLocks = new HashSet<String>();
-    private int mWakeLockBlockingEnabled;
 
     // A bitfield that summarizes the state of all active wakelocks.
     private int mWakeLockSummary;
@@ -548,12 +542,6 @@ public final class PowerManagerService extends IPowerManager.Stub
                     Settings.System.AUTO_BRIGHTNESS_RESPONSIVENESS),
                     false, mSettingsObserver, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.WAKELOCK_BLOCKING_ENABLED),
-                    false, mSettingsObserver, UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.WAKELOCK_BLOCKING_LIST),
-                    false, mSettingsObserver, UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.BUTTON_BRIGHTNESS),
                     false, mSettingsObserver, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
@@ -613,18 +601,6 @@ public final class PowerManagerService extends IPowerManager.Stub
                 Settings.System.SYSTEM_POWER_CRT_MODE,
                 1, UserHandle.USER_CURRENT);
 
-
-        mWakeLockBlockingEnabled = Settings.System.getIntForUser(resolver,
-                Settings.System.WAKELOCK_BLOCKING_ENABLED,
-                0, UserHandle.USER_CURRENT);
-
-        String blockedWakelockList = Settings.System.getStringForUser(resolver,
-                Settings.System.WAKELOCK_BLOCKING_LIST,
-                UserHandle.USER_CURRENT);
-        setBlockedWakeLocks(blockedWakelockList);
-
-        Slog.d(TAG, "mWakeLockBlockingEnabled=" + mWakeLockBlockingEnabled +
-                     " blockedWakelockList=" + blockedWakelockList);
 
         final int oldScreenBrightnessSetting = mScreenBrightnessSetting;
         mScreenBrightnessSetting = Settings.System.getIntForUser(resolver,
@@ -716,18 +692,6 @@ public final class PowerManagerService extends IPowerManager.Stub
                         + ", tag=\"" + tag + "\", ws=" + ws + ", uid=" + uid + ", pid=" + pid);
             }
 
-            boolean blockWakelock = false;
-
-            if (!mSeenWakeLocks.contains(tag)){
-                mSeenWakeLocks.add(tag);
-            }
-
-            if (mWakeLockBlockingEnabled == 1){
-                if (mBlockedWakeLocks.contains(tag)){
-                    blockWakelock = true;
-                }
-            }
-
             WakeLock wakeLock;
             int index = findWakeLockIndexLocked(lock);
             if (index >= 0) {
@@ -749,18 +713,9 @@ public final class PowerManagerService extends IPowerManager.Stub
                 mWakeLocks.add(wakeLock);
             }
 
-            wakeLock.setIsBlocked(blockWakelock);
-
-            if (!wakeLock.isBlocked()){
-                applyWakeLockFlagsOnAcquireLocked(wakeLock);
-                mDirty |= DIRTY_WAKE_LOCKS;
-                updatePowerStateLocked();
-            } else {
-                Slog.d(TAG, "acquireWakeLockInternal: blocked lock=" + Objects.hashCode(lock)
-                        + ", flags=0x" + Integer.toHexString(flags)
-                        + ", tag=\"" + tag + "\", ws=" + ws + ", uid=" + uid + ", pid=" + pid);
-
-            }
+            applyWakeLockFlagsOnAcquireLocked(wakeLock);
+            mDirty |= DIRTY_WAKE_LOCKS;
+            updatePowerStateLocked();
         }
     }
 
@@ -2701,7 +2656,6 @@ public final class PowerManagerService extends IPowerManager.Stub
         public final int mOwnerUid;
         public final int mOwnerPid;
         public boolean mNotifiedAcquired;
-        private boolean mIsBlocked;
 
         public WakeLock(IBinder lock, int flags, String tag, String packageName,
                 WorkSource workSource, int ownerUid, int ownerPid) {
@@ -2788,14 +2742,6 @@ public final class PowerManagerService extends IPowerManager.Stub
                 result += " ON_AFTER_RELEASE";
             }
             return result;
-        }
-
-        public void setIsBlocked(boolean value){
-            mIsBlocked = value;
-        }
-
-        public boolean isBlocked(){
-            return mIsBlocked;
         }
     }
 
@@ -2931,31 +2877,6 @@ public final class PowerManagerService extends IPowerManager.Stub
         public String toString() {
             synchronized (this) {
                 return "blanked=" + mBlanked;
-            }
-        }
-    }
-
-    @Override
-    public String getSeenWakeLocks(){
-        StringBuffer buffer = new StringBuffer();
-        Iterator<String> nextWakeLock = mSeenWakeLocks.iterator();
-        while (nextWakeLock.hasNext()){
-            String wakeLockTag = nextWakeLock.next();
-            buffer.append(wakeLockTag + "|");
-        }
-        if(buffer.length()>0){
-            buffer.deleteCharAt(buffer.length() - 1);
-        }
-        return buffer.toString();
-    }
-
-    private void setBlockedWakeLocks(String wakeLockTagsString) {
-        mBlockedWakeLocks = new HashSet<String>();
-
-        if (wakeLockTagsString!=null && wakeLockTagsString.length()!=0){
-            String[] parts = wakeLockTagsString.split("\\|");
-            for(int i = 0; i < parts.length; i++){
-                mBlockedWakeLocks.add(parts[i]);
             }
         }
     }
