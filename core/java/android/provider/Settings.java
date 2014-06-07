a2f2fff13988e2461eb0fe7a0889cbfbc5df9b6c
willl0344 <wgangers@gmail.com>
2014-06-06 22:33:02 -0400

    Revert "Privacy Guard Slim version (1/2)"
    This reverts commit cee1ad42db95ba1ecc1a3f589ed4d69fdc6c947e.
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/slim_strings.xml
    	core/res/res/values/slim_symbols.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -492,19 +492,6 @@ public final class Settings {
 
     /**
      * @hide
-     * Activity Action: Show the "app ops" details screen.
-     * <p>
-     * Input: The Intent's data URI specifies the application package name
-     * to be shown, with the "package" scheme.  That is "package:com.my.app".
-     * <p>
-     * Output: Nothing.
-     */
-    @SdkConstant(SdkConstantType.ACTIVITY_INTENT_ACTION)
-    public static final String ACTION_APP_OPS_DETAILS_SETTINGS =
-            "android.settings.APP_OPS_DETAILS_SETTINGS";
-
-    /**
-     * @hide
      * Activity Action: Show the "app ops" settings screen.
      * <p>
      * Input: Nothing.
@@ -6180,18 +6167,6 @@ public final class Settings {
         public static final String SMS_DEFAULT_APPLICATION = "sms_default_application";
 
         /**
-         * Whether newly installed apps should run with privacy guard by default
-         * @hide
-         */
-        public static final String PRIVACY_GUARD_DEFAULT = "privacy_guard_default";
-
-        /**
-         * Whether privacy guard notification should show.
-         * @hide
-         */
-        public static final String PRIVACY_GUARD_NOTIFICATION = "privacy_guard_notification";
-
-        /**
          * Name of a package that the current user has explicitly allowed to see all of that
          * user's notifications.
          *
@@ -6276,8 +6251,6 @@ public final class Settings {
             UI_NIGHT_MODE,
             UI_THEME_MODE,
             UI_THEME_AUTO_MODE,
-            PRIVACY_GUARD_DEFAULT,
-            PRIVACY_GUARD_NOTIFICATION,
             ADVANCED_REBOOT
         };
 
