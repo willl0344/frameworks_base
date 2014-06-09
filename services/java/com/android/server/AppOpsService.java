4dcd228b6fe2f88f60adbb879ab5190dff635715
willl0344 <wgangers@gmail.com>
2014-06-09 18:17:54 -0400

    Revert "Use MODE_ASK as the default ops mode in Privacy Guard"
    This reverts commit 6ae4e51e9a5bd3623aab85a892394da4028e9e90.

--- services/java/com/android/server/AppOpsService.java
+++ services/java/com/android/server/AppOpsService.java
@@ -1172,7 +1172,7 @@ public class AppOpsService extends IAppOpsService.Stub {
             for (AppOpsManager.OpEntry op : packageOps.get(0).getOps()) {
                 pgDetect = false;
                 if (checkOperation(op.getOp(), uid, packageName)
-                    == AppOpsManager.MODE_ASK) {
+                    == AppOpsManager.MODE_IGNORED) {
                     for (int pgOp : privacyGuardOps) {
                         if (AppOpsManager.opToSwitch(op.getOp()) == pgOp) {
                             privacyGuardOpsHelper.remove((Integer) pgOp);
