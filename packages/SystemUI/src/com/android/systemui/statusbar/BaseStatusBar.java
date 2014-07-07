2027e3dfc0dd3f88e9cb7e2406266fd23f147a2b
willl0344 <wgangers@gmail.com>
2014-07-07 09:51:17 -0400

    Revert "HOVER [1/2]"
    This reverts commit 992d1f71103872beae60b456522c043b2218b5c4.
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values/pa_strings.xml

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -37,7 +37,6 @@ import android.content.pm.ResolveInfo;
 import android.content.res.Configuration;
 import android.content.ServiceConnection;
 import android.database.ContentObserver;
-import android.graphics.drawable.Drawable;
 import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Build;
@@ -87,8 +86,6 @@ import com.android.systemui.RecentsComponent;
 import com.android.systemui.SearchPanelView;
 import com.android.systemui.SystemUI;
 import com.android.systemui.slimrecent.RecentController;
-import com.android.systemui.statusbar.notification.Hover;
-import com.android.systemui.statusbar.notification.HoverCling;
 import com.android.systemui.statusbar.phone.Ticker;
 import com.android.systemui.statusbar.phone.KeyguardTouchDelegate;
 import com.android.systemui.statusbar.phone.NavigationBarOverlay;
@@ -138,9 +135,6 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     public static final int EXPANDED_LEAVE_ALONE = -10000;
     public static final int EXPANDED_FULL_OPEN = -10001;
-
-    public static final int HOVER_DISABLED = 0;
-    public static final int HOVER_ENABLED = 1;
 
     protected CommandQueue mCommandQueue;
     protected INotificationManager mNotificationManager;
@@ -221,12 +215,6 @@ public abstract class BaseStatusBar extends SystemUI implements
     // Peek
     protected Peek mPeek;
 
-    // Hover
-    protected Hover mHover;
-    protected int mHoverState;
-    protected ImageView mHoverButton;
-    protected HoverCling mHoverCling;
-
     // UI-specific methods
 
     /**
@@ -268,12 +256,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         return mNotificationData;
     }
 
-    public RemoteViews.OnClickHandler getNotificationClickHandler() {
-        return mOnClickHandler;
-    }
-
     private ContentObserver mProvisioningObserver = new ContentObserver(new Handler()) {
-
         @Override
         public void onChange(boolean selfChange) {
             final boolean provisioned = 0 != Settings.Global.getInt(
@@ -291,12 +274,6 @@ public abstract class BaseStatusBar extends SystemUI implements
             if (DEBUG) {
                 Log.v(TAG, "Notification click handler invoked for intent: " + pendingIntent);
             }
-
-            // User is clicking a button inside the notification, stop countdowns and
-            // restart override one depending on notification expansion.
-            // We just ignore incoming call case cause is handled differently, as soon as dialer shows, is gone.
-            mHover.processOverridingQueue(mHover.isExpanded());
-
             final boolean isActivity = pendingIntent.isActivity();
             if (isActivity) {
                 try {
@@ -374,12 +351,9 @@ public abstract class BaseStatusBar extends SystemUI implements
         mStatusBarContainer = new FrameLayout(mContext);
 
         mPeek = new Peek(this, mContext);
-        mHover = new Hover(this, mContext);
-        mHoverCling = new HoverCling(mContext);
         mNotificationHelper = new NotificationHelper(this, mContext);
 
         mPeek.setNotificationHelper(mNotificationHelper);
-        mHover.setNotificationHelper(mNotificationHelper);
 
         // Connect in to the status bar manager service
         StatusBarIconList iconList = new StatusBarIconList();
@@ -445,33 +419,6 @@ public abstract class BaseStatusBar extends SystemUI implements
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_USER_SWITCHED);
         mContext.registerReceiver(mBroadcastReceiver, filter);
-
-        mContext.getContentResolver().registerContentObserver(
-                Settings.System.getUriFor(Settings.System.HOVER_STATE),
-                        false, new ContentObserver(new Handler()) {
-            @Override
-            public void onChange(boolean selfChange) {
-                updateHoverState();
-            }
-        });
-
-        updateHoverState();
-
-        mContext.getContentResolver().registerContentObserver(
-                Settings.System.getUriFor(Settings.System.DIALPAD_STATE),
-                        false, new ContentObserver(new Handler()) {
-            @Override
-            public void onChange(boolean selfChange) {
-                boolean showing = Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.DIALPAD_STATE, 0) != 0;
-                if(showing) mHover.dismissHover(false, false);
-            }
-        });
-    }
-
-    public Hover getHoverInstance() {
-        if(mHover == null) mHover = new Hover(this, mContext);
-        return mHover;
     }
 
     private void initPieController() {
@@ -513,17 +460,6 @@ public abstract class BaseStatusBar extends SystemUI implements
         if (mEdgeGestureManager != null) {
             mEdgeGestureManager.setOverwriteImeIsActive(enabled);
         }
-    }
-
-    protected void updateHoverState() {
-        mHoverState = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.HOVER_STATE, HOVER_DISABLED);
-
-        mHoverButton.setImageResource(mHoverState != HOVER_DISABLED
-                ? R.drawable.ic_notify_hover_pressed
-                        : R.drawable.ic_notify_hover_normal);
-
-        mHover.setHoverActive(mHoverState == HOVER_ENABLED);
     }
 
     public void userSwitched(int newUserId) {
@@ -1243,10 +1179,6 @@ public abstract class BaseStatusBar extends SystemUI implements
 
         mPeek.removeNotification(entry.notification);
 
-        // If a notif is on hover list or currently showed in hover,
-        // remove (hide) it if system does.
-        mHover.removeNotification(entry);
-
         return entry.notification;
     }
 
@@ -1271,8 +1203,8 @@ public abstract class BaseStatusBar extends SystemUI implements
             handleNotificationError(key, notification, "Couldn't create icon: " + ic);
             return null;
         }
-        NotificationData.Entry entry = new NotificationData.Entry(key, notification, iconView);
         // Construct the expanded view.
+        NotificationData.Entry entry = new NotificationData.Entry(key, notification, iconView);
         if (!inflateViews(entry, mPile)) {
             handleNotificationError(key, notification, "Couldn't expand RemoteViews for: "
                     + notification);
@@ -1300,14 +1232,7 @@ public abstract class BaseStatusBar extends SystemUI implements
             } else {
                 mPeek.addNotification(entry.notification);
             }
-            mHover.addStatusBarNotification(entry.notification);
         } else {
-            // screen on - check if hover is enabled
-            if (mNotificationHelper.isHoverEnabled()) {
-                mHover.setNotification(entry, false);
-            } else {
-                mHover.addStatusBarNotification(entry.notification);
-            }
             mPeek.addNotification(entry.notification);
         }
     }
@@ -1316,7 +1241,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         addNotificationViews(createNotificationViews(key, notification));
     }
 
-    public void updateExpansionStates() {
+    protected void updateExpansionStates() {
         int N = mNotificationData.size();
         for (int i = 0; i < N; i++) {
             NotificationData.Entry entry = mNotificationData.get(i);
@@ -1338,14 +1263,6 @@ public abstract class BaseStatusBar extends SystemUI implements
         }
     }
 
-    public void animateStatusBarOut() {
-        // should be overridden
-    }
-
-    public void animateStatusBarIn() {
-        // should be overridden
-    }
-
     protected abstract void haltTicker();
     protected abstract void setAreThereNotifications();
     protected abstract void updateNotificationIcons();
@@ -1353,7 +1270,6 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected abstract void updateExpandedViewPos(int expandedPosition);
     protected abstract int getExpandedViewMaxHeight();
     protected abstract boolean shouldDisableNavbarGestures();
-    public abstract boolean isExpandedVisible();
 
     protected boolean isTopNotification(ViewGroup parent, NotificationData.Entry entry) {
         return parent != null && parent.indexOfChild(entry.row) == 0;
@@ -1411,10 +1327,9 @@ public abstract class BaseStatusBar extends SystemUI implements
                 && notification.getScore() == oldNotification.getScore();
                 // score now encompasses/supersedes isOngoing()
 
-        boolean updateTicker = (notification.getNotification().tickerText != null
+        boolean updateTicker = notification.getNotification().tickerText != null
                 && !TextUtils.equals(notification.getNotification().tickerText,
-                        oldEntry.notification.getNotification().tickerText)) &&
-                        (mHoverState == HOVER_DISABLED);
+                        oldEntry.notification.getNotification().tickerText);
         boolean isTopAnyway = isTopNotification(rowParent, oldEntry);
         if (contentsUnchanged && bigContentsUnchanged && (orderUnchanged || isTopAnyway)) {
             if (DEBUG) Log.d(TAG, "reusing notification for key: " + key);
@@ -1519,15 +1434,7 @@ public abstract class BaseStatusBar extends SystemUI implements
             } else {
                 mPeek.addNotification(entry.notification);
             }
-            mHover.addStatusBarNotification(entry.notification);
         } else {
-            // screen on - check if hover is enabled
-            if (mNotificationHelper.isHoverEnabled()) {
-                mHover.setNotification(entry, true);
-            } else {
-                // We pass this to hover here only if it doesn't show
-                mHover.addStatusBarNotification(entry.notification);
-            }
             mPeek.addNotification(entry.notification);
         }
     }
--- services/java/com/android/server/NotificationManagerService.java
+++ services/java/com/android/server/NotificationManagerService.java
@@ -206,11 +206,10 @@ public class NotificationManagerService extends INotificationManager.Stub
     private HashSet<String> mEnabledListenerPackageNames = new HashSet<String>();
 
     // Notification control database. For now just contains disabled packages.
-    private AtomicFile mPolicyFile, mPeekPolicyFile, mFloatingModePolicyFile, mHoverPolicyFile;
+    private AtomicFile mPolicyFile, mPeekPolicyFile, mFloatingModePolicyFile;
     private HashSet<String> mBlockedPackages = new HashSet<String>();
     private HashSet<String> mPeekBlacklist = new HashSet<String>();
     private HashSet<String> mFloatingModeBlacklist = new HashSet<String>();
-    private HashSet<String> mHoverBlacklist = new HashSet<String>();
 
     private static final int DB_VERSION = 1;
 
@@ -224,7 +223,6 @@ public class NotificationManagerService extends INotificationManager.Stub
     private static final String NOTIFICATION_POLICY = "notification_policy.xml";
     private static final String PEEK_POLICY = "peek_policy.xml";
     private static final String FLOATING_MODE_POLICY = "floating_mode_policy.xml";
-    private static final String HOVER_POLICY = "hover_policy.xml";
 
     private final ArrayList<NotificationScorer> mScorers = new ArrayList<NotificationScorer>();
 
@@ -470,16 +468,6 @@ public class NotificationManagerService extends INotificationManager.Stub
         }
     }
 
-    private void loadHoverBlockDb() {
-        synchronized(mHoverBlacklist) {
-            if (mHoverPolicyFile == null) {
-                mHoverPolicyFile = new AtomicFile(new File(SYSTEM_FOLDER, HOVER_POLICY));
-                mHoverBlacklist.clear();
-                readPolicy(mHoverPolicyFile, TAG_BLOCKED_PKGS, mHoverBlacklist);
-            }
-        }
-    }
-
     private void writeBlockDb() {
         synchronized(mBlockedPackages) {
             FileOutputStream outfile = null;
@@ -582,40 +570,6 @@ public class NotificationManagerService extends INotificationManager.Stub
         }
     }
 
-    private void writeHoverBlockDb() {
-        synchronized(mHoverBlacklist) {
-            FileOutputStream outfile = null;
-            try {
-                outfile = mHoverPolicyFile.startWrite();
-
-                XmlSerializer out = new FastXmlSerializer();
-                out.setOutput(outfile, "utf-8");
-
-                out.startDocument(null, true);
-
-                out.startTag(null, TAG_BODY); {
-                    out.attribute(null, ATTR_VERSION, String.valueOf(DB_VERSION));
-                    out.startTag(null, TAG_BLOCKED_PKGS); {
-                        // write all known network policies
-                        for (String pkg : mHoverBlacklist) {
-                            out.startTag(null, TAG_PACKAGE); {
-                                out.attribute(null, ATTR_NAME, pkg);
-                            } out.endTag(null, TAG_PACKAGE);
-                        }
-                    } out.endTag(null, TAG_BLOCKED_PKGS);
-                } out.endTag(null, TAG_BODY);
-
-                out.endDocument();
-
-                mHoverPolicyFile.finishWrite(outfile);
-            } catch (IOException e) {
-                if (outfile != null) {
-                    mHoverPolicyFile.failWrite(outfile);
-                }
-            }
-        }
-    }
-
     public void setPeekBlacklistStatus(String pkg, boolean status) {
         if (status) {
             mPeekBlacklist.add(pkg);
@@ -634,25 +588,12 @@ public class NotificationManagerService extends INotificationManager.Stub
         writeFloatingModeBlockDb();
     }
 
-    public void setHoverBlacklistStatus(String pkg, boolean status) {
-        if (status) {
-            mHoverBlacklist.add(pkg);
-        } else {
-            mHoverBlacklist.remove(pkg);
-        }
-        writeHoverBlockDb();
-    }
-
     public boolean isPackageAllowedForPeek(String pkg) {
         return !mPeekBlacklist.contains(pkg);
     }
 
     public boolean isPackageAllowedForFloatingMode(String pkg) {
         return !mFloatingModeBlacklist.contains(pkg);
-    }
-
-    public boolean isPackageAllowedForHover(String pkg) {
-        return !mHoverBlacklist.contains(pkg);
     }
 
     /**
@@ -692,7 +633,6 @@ public class NotificationManagerService extends INotificationManager.Stub
         writeBlockDb();
         writePeekBlockDb();
         writeFloatingModeBlockDb();
-        writeHoverBlockDb();
     }
 
 
@@ -1695,7 +1635,6 @@ public class NotificationManagerService extends INotificationManager.Stub
         loadBlockDb();
         loadPeekBlockDb();
         loadFloatingModeBlockDb();
-        loadHoverBlockDb();
 
         PackageManager pm = mContext.getPackageManager();
         for (String pkg : mBlockedPackages) {
